403d63f9c88c0e521a1c8a11e21042cadd01a1a7
GiselleTodd <43821673+GiselleTodd@users.noreply.github.com>
2018-11-07 20:34:05 -0500

    No extra feature added, final puzzle

--- fifteen.js
+++ fifteen.js
@@ -1,246 +1,219 @@
-window.onload = () => {
 
-  let whole = document.getElementById("puzzlearea");
-  let segs = document.querySelectorAll("#puzzlearea div");
-  let x = 0;
-  let y = 0;
-  let shufflebutton = document.getElementById("shufflebutton");
-  let blankSpaceX  = "300px";
-  let blankSpaceY  = "300px";
-  
+window.onload =() =>
+{
+ 
+ let puzzleArea = document.getElementById("puzzlearea");
+ let seg = document.querySelectorAll("#puzzlearea div")
+ let shufflebutton = document.getElementById("shufflebutton")
+ 
+ let x=0;
+ let y =0;
+ let count =0;
+
+ let blankSpaceX= '300px';
+ let blankSpaceY= '300px';
 
 
 
-for (let i = 0; i < segs.length; i++){ 
-
-    segs[i].style.backgroundImage="url('background.jpg')";
-    segs[i].classList.add("puzzlepiece");
-    segs[i].style.top = y+"px";
-    segs[i].style.left = x+"px";
-    segs[i].style.backgroundPosition = `${-x}px ${-y}px`;
-
-    x += 100;
-    
-
-    if(x == 400){
-      x=0
-      y += 100;
+ 
+ for (let i=0; i< seg.length; i++)
+  {
+   seg[i].classList.add("puzzlepiece");
+   seg[i].style.left = x+"px";  
+   seg[i].style.top = y+"px";
+   //Line of code to help position of image in each tile
+   seg[i].style.backgroundPosition = `${-x}px ${-y}px`;
+   x +=100;
+   count +=1;
+   if (count%4==0){
+    y+=100;
+    x=0;
+   }
+   
+   seg[i].addEventListener("mouseover",highlightTile);
+   seg[i].addEventListener("mouseout",undoHiglight);
+   seg[i].addEventListener("click",moveTile);
+   
     }
 
-    segs[i].addEventListener("mouseover", changeTileColour);
-    segs[i].addEventListener("mouseout", removeChange);
-    segs[i].addEventListener("click", slideTile);
-  
+     shufflebutton.addEventListener("click",shuffle);
+     restartbutton.addEventListener("click",restart);
 
+ function highlightTile()
+   {
+    if(move(parseInt(this.innerHTML)))
+    {
+     this.style.border = "2px solid red";
+     this.style.color = "#006600";
     }
+   };
+ function undoHiglight()
+   {
+    this.style.border = "2px solid black";
+    this.style.color = "#000000";
+   };
 
 
-
-
-  shufflebutton.addEventListener("click", shuffle);
-
-
-    function changeTileColour(){
-      this.style.border = "2px solid red";
-      this.style.color = "#006600";
-
+ //Function to allow tiles to chagned their position
+ function moveTile()
+   {
+    if(move(parseInt(this.innerHTML)))
+    {
+     swap(this.innerHTML-1);
+     if(finish())
+     {
+      Gamewin();
+     }
+     return;
     }
-
-
-    function removeChange(){
-      this.style.border = "2px solid black";
-      this.style.color = "black";
-      this.style.textdecoration = "underline";
-
-    }
-//change function names accordingly; misleading/confusing function names
-    function shuffle(){
-      for (let i = 0; i < 250; i++){
-
-        let rand = parseInt(Math.random() * 100) % 4;
-          if (rand == 0){
-            let mve = checkTopMove(blankSpaceX, blankSpaceY);
-
-            if (mve != -1){
-                swap(mve);
+   };
+ 
+ function shuffle()
+ {
+  for (var i=0; i<250; i++)
+        {
+            let rand = parseInt(Math.random()* 100) %4;
+            if (rand == 0)
+            {
+                let mve = checkUp(blankSpaceX,blankSpaceY);
+                if ( mve != -1)
+                {
+                    swap(mve);
+                }
             }
-          }
-
-          if (rand == 1){
-            let mve = checkBelowMove(blankSpaceX, blankSpaceY);
-
-            if (mve != -1){
-                swap(mve);
+            if (rand == 1)
+            {
+                let mve = checkDown(blankSpaceX,blankSpaceY);
+                if ( mve != -1) 
+                {
+                    swap(mve);
+                }
             }
-          }
-
-          if (rand == 2){
-            let mve = checkLeftMove(blankSpaceX, blankSpaceY);
-
-            if (mve != -1){
-                swap(mve);
+            if (rand == 2)
+            {
+                let mve = checkLeft(blankSpaceX,blankSpaceY);
+                if ( mve != -1)
+                {
+                    swap(mve);
+                }
             }
-          }
-
-          if (rand == 3){
-            let mve = checkRightMove(blankSpaceX, blankSpaceY);
-
-            if (mve != -1){
-                swap(mve);
+            if (rand == 3)
+            {
+                let mve = checkRight(blankSpaceX,blankSpaceY);
+                if (mve != -1)
+                {
+                    swap(mve);
+                }
             }
-          }
-        
-
-
-           
-      }
-
-    }
-
-
-
-    function slideTile(){
-        if(canMove(parseInt(this.innerHTML))){
-          swap(this.innerHtML -1);
         }
+ }
+ 
+ 
+ function swap (pos)
+ {
+  let temp = seg[pos].style.top;
+  seg[pos].style.top= blankSpaceY;
+  blankSpaceY = temp;
+  temp=seg[pos].style.left;
+  seg[pos].style.left = blankSpaceX;
+  blankSpaceX = temp;
+ }
 
-    }
-
-
-
-     function swap(pos){
-
-        let temp = segs[pos].style.top;
-        segs[pos].style.top = blankSpaceY;
-        blankSpaceY = temp;
-
-        temp = segs[pos].style.left;
-        segs[pos].style.left = blankSpaceX;
-        blankSpaceX = temp;
-
-    }
-
-    function canMove(pos){
-          if (checkLeftMove(blankSpaceX, blankSpaceY) == (pos-1)){
-            return true;
-          }
-
-          if (checkRightMove(blankSpaceX, blankSpaceY) == (pos-1)){
-            return true;
-          
-          }
-
-          if (checkTopMove(blankSpaceX, blankSpaceY) == (pos-1)){
-            return true;
-          
-          }
-
-          if (checkBelowMove(blankSpaceX, blankSpaceY) == (pos-1)){
-            return true;
-          
-          }
-
-      }
-
-
-    function checkLeftMove(x, y){
-      let pieceX = parseInt(x);
-      let pieceY = parseInt(y);
-
-
-      if (pieceX == 0){
-
-        for (let i = 0; i < segs.length; i++){
-            if ( parseInt(segs[i].style.left)+ 100 == pieceX && parseInt(segs[i].style.top) == pieceY ){
-
-                return i;
-
-
-            }
-
-        }
-
-
-      } else  {
-
-        return -1;
-
-      }
-
-    }
-
-    function checkRightMove(x,y){
-      let pieceX = parseInt(x);
-      let pieceY = parseInt(y);
-
-      if (pieceX < 300){
-
-        for (let i = 0; i < segs.length ; i++){
-         
-            if(parseInt(segs[i].style.left) - 100 == pieceX && parseInt(segs[i].style.top) == pieceY){
-
-              return i;
-            }
-
-        }
-
-      } else {
-
-        return -1;
-      }
-
-    }
-
-    function checkTopMove(x,y){
-      let pieceX = parseInt(x);
-      let pieceY = parseInt(y);
-
-      if (pieceY > 0){
-
-        for (let i = 0; i < segs.length ; i++){
-         
-            if(parseInt(segs[i].style.top) + 100 == pieceY && parseInt(segs[i].style.topleft) == pieceX){
-
-              return i;
-            }
-
-        }
-
-      } else {
-
-        return -1;
-      }
-
-    }
-
-    function checkBelowMove(x,y){
-      let pieceX = parseInt(x);
-      let pieceY = parseInt(y);
-
-      if (pieceY < 300){
-
-        for (let i = 0; i < segs.length ; i++){
-         
-            if(parseInt(segs[i].style.top) - 100 == pieceY && parseInt(segs[i].style.left) == pieceX){
-
-              return i;
-            }
-
-        }
-
-      } else {
-
-        return -1;
-      }
-
-
-
-
-    }
-
-
-
-
-
+ 
+ function move(pos)
+ {
+  if (checkLeft(blankSpaceX,blankSpaceY) == (pos-1))
+  {
+   return true;
   }
+  if(checkDown(blankSpaceX,blankSpaceY) == (pos-1))
+  {
+   return true;
+  }
+  if(checkUp(blankSpaceX,blankSpaceY) == (pos-1))
+  {
+   return true;
+  }
+  if(checkRight(blankSpaceX,blankSpaceY) == (pos-1))
+  {
+   return true;
+  }
+ }
 
+ function checkUp (x, y)
+ {
+     var xx = parseInt(x);
+     var yy = parseInt(y);
+     if (yy > 0)
+     {
+         for (var i=0; i<seg.length; i++)
+         {
+             if (parseInt(seg[i].style.top) + 100 == yy && parseInt(seg[i].style.left) == xx) 
+             {
+                 return i;
+             }
+         } 
+     }
+     else 
+     {
+         return -1;
+     }
+ }
+ function checkDown (x, y)
+ {
+     var xx = parseInt(x);
+     var yy = parseInt(y);
+     if (yy < 300)
+     {
+         for (var i=0; i<seg.length; i++)
+         {
+             if (parseInt(seg[i].style.top) - 100 == yy && parseInt(seg[i].style.left) == xx) 
+             {
+                 return i;
+             }
+         }
+     } 
+     else
+     {
+         return -1;
+     } 
+ }
+ function checkLeft(x, y)
+ {
+     var xx = parseInt(x);
+     var yy = parseInt(y);
+     if (xx > 0)
+     {
+         for (var i = 0; i < seg.length; i++) 
+         {
+             if (parseInt(seg[i].style.left) + 100 == xx && parseInt(seg[i].style.top) == yy)
+             {
+                 return i;
+             } 
+         }
+     }
+     else 
+     {
+         return -1;
+     }
+ }
+ function checkRight (x, y)
+ {
+     var xx = parseInt(x);
+     var yy = parseInt(y);
+     if (xx < 300)
+     {
+         for (var i =0; i<seg.length; i++)
+         {
+             if (parseInt(seg[i].style.left) - 100 == xx && parseInt(seg[i].style.top) == yy) 
+             {
+                 return i;
+             }
+         }
+     }
+     else
+     {
+         return -1;
+     } 
+ }
+}
